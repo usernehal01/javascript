b3dabaf8b7227b9bbbf833ee265848fca6780e3c
Nehal chaudhary <thestonershreddedking@gmail.com>
2025-06-10 14:01:37 +0000

    another commit

--- test.js
+++ test.js
@@ -1,1 +1,8 @@
-console.log("hi")
+let object = "abcdjkwad";
+for (const key in object) {
+    if (Object.prototype.hasOwnProperty.call(object, key)) {
+        const element = object[key];
+        console.log(element);
+        
+    }
+}
